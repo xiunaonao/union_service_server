355fe649339bf9dc9f6f414bf5a98d6613f28c03
xiunaonao <onlymyalways@163.com>
2019-04-26 11:37:29 +0800

    wechat

--- routes/jt.js
+++ routes/jt.js
@@ -9,9 +9,9 @@ router.get('/', (req, res, next)=>{
 });
 
 router.get('/wechat',(req,res,next)=>{
-	let url=encodeURIComponent('http://service.123zou.com/#/jt/signin').toLocaleLowerCase()
+	let url=encodeURIComponent('http://service.123zou.com/#/jt/signin/0').toLocaleLowerCase()
 	let appid='wx7bc344f62f4fdaa3'
-	res.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`)
+	res.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`)
 })
 
 router.get('/wechat_user',(req,res,next)=>{
